a67932edb6ad16c812b27637bc2a91598a2a187f
JohnWoods11 <itwasabet@hotmail.com>
2020-06-25 00:40:57 +0100

    Updates

---
+++ precache-manifest.2f29c5c7a82d728bb72f761ef71f1502.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "195f980efe5a46e23ceca9d769c2276b",
+    "url": "/fishv2/index.html"
+  },
+  {
+    "revision": "47b6b3542a9c829091e0",
+    "url": "/fishv2/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "f1e07ba93d03e53b72bf",
+    "url": "/fishv2/static/css/main.04a99545.chunk.css"
+  },
+  {
+    "revision": "47b6b3542a9c829091e0",
+    "url": "/fishv2/static/js/2.0ae813a3.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/fishv2/static/js/2.0ae813a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1e07ba93d03e53b72bf",
+    "url": "/fishv2/static/js/main.8a3a7b86.chunk.js"
+  },
+  {
+    "revision": "dd66605890d77192eed4",
+    "url": "/fishv2/static/js/runtime-main.fbb96232.js"
+  }
+]);
